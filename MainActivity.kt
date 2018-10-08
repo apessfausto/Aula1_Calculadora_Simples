af5082d9f0e866b28ca99b2af654573c811fb35a
Fausto Caires Goncalves <psicotattoo@hotmail.com>
2018-10-08 08:52:04 -0300

    Initial commit

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aula1.fausto.calculadora_simples.aula1_calculadora_simples
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
